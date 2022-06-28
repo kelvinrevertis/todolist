0c25cccfbff8fd7d703c31d43c3029e6dfb973a9
kelvinrevertis <kelvinrev2@hotmail.com>
2022-06-27 21:14:47 -0300

    Second Commit: li>input(checkbox) ok

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 function addTask() {
+
     if (document.getElementById('task').value == "") {
         alert("Erro: lista vazia")
     } else {
+        //valor digitado no input
         let task = document.getElementById('task').value
 
+        //criar li
         let item = document.createElement('li')
-        item.innerText = task 
 
         let check = document.createElement('input')
         check.type = "checkbox"
-        check.value = item
+
         item.appendChild(check)
+
+        item.appendChild(document.createTextNode(task))
+
         document.getElementById("task-list").appendChild(item)
 
     }
 }
+
+/*function addTask(){
+
+    let task = document.getElementById('task').value
+
+    let item = `<li><input type="checkbox"> ${task}</li>`
+    item.setAtribute = "input"
+    
+    // item.appendChild(document.createTextNode(task))
+
+    let list = document.getElementById("task-list")
+
+    list.innerHTML = item
+
+}*/
