176ee3c066f4107ac11ff07ef0dffe10664fa55b
kelvinrevertis <kelvinrev2@hotmail.com>
2022-06-28 21:41:41 -0300

    commit 3.1

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ function addTask() {
         let item = document.createElement('li')
 
         let check = document.createElement('input')
+        check.setAttribute("id","myCheck")
+        check.setAttribute("onChange","createDel()")
         check.type = "checkbox"
 
         let text = document.createElement('label')
@@ -19,10 +21,12 @@ function addTask() {
         item.appendChild(text)
 
         document.getElementById("task-list").appendChild(item)
+
         let removeBtn = document.createElement("button")
         removeBtn.setAttribute("onClick", "removeTask(this)")
         removeBtn.setAttribute("class", "fa fa-trash")
         removeBtn.setAttribute("id","test-check")
+        //removeBtn.setAttribute("style","display:block")
 
         item.appendChild(removeBtn)
     }
@@ -33,6 +37,12 @@ function removeTask(button) {
     let itemDel = button.parentNode
 
     document.getElementById("task-list").removeChild(itemDel)
+}
+
+/*function createDel() {
+    let showBtn = document.getElementById("myCheck")
+
+    showBtn.style.display = "block" 
 }
 
 /*function addTask(){
