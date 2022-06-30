11cdb1b7203f827f9c7a45171723fe65acaae0df
kelvinrevertis <kelvinrev2@hotmail.com>
2022-06-30 20:04:27 -0300

    Commit 4.0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
 function addTask() {
+    if (document.getElementById('task').value == "") {
+        alert("Erro: lista vazia")
+    } else {
+        let task = document.getElementById('task').value
+
+        let removeBtn = document.createElement("button")
+        removeBtn.setAttribute("onClick", "removeTask(this)")
+        removeBtn.setAttribute("class", "fa fa-trash")
+
+        const item = document.createElement('li')
+        item.innerHTML = `
+            <input type="checkbox">
+            <label>${task}</label>
+            `
+
+        item.appendChild(removeBtn)
+
+        document.getElementById('task-list').appendChild(item)
+    }
+    document.getElementById('task').value = ""
+}
+
+function removeTask(button) {
+    let itemDel = button.parentNode
+
+    document.getElementById("task-list").removeChild(itemDel)
+}
+
+
+/*var save
+
+
+function inicia(){
+    if(localStorage.save){
+        document.getElementById("task-list").innerText = save
+    }
+}
+
+function addTask() {
 
     if (document.getElementById('task').value == "") {
         alert("Erro: lista vazia")
@@ -10,9 +49,8 @@ function addTask() {
         let item = document.createElement('li')
 
         let check = document.createElement('input')
-        check.setAttribute("id","myCheck")
-        check.setAttribute("onChange","createDel()")
         check.type = "checkbox"
+        
 
         let text = document.createElement('label')
         text.appendChild(document.createTextNode(task))
@@ -25,12 +63,13 @@ function addTask() {
         let removeBtn = document.createElement("button")
         removeBtn.setAttribute("onClick", "removeTask(this)")
         removeBtn.setAttribute("class", "fa fa-trash")
-        removeBtn.setAttribute("id","test-check")
-        //removeBtn.setAttribute("style","display:block")
 
         item.appendChild(removeBtn)
 
+        localStorage.save = item
+
         document.getElementById('task').value=""
+
     }
 }
 
@@ -41,11 +80,13 @@ function removeTask(button) {
     document.getElementById("task-list").removeChild(itemDel)
 }
 
-/*function createDel() {
-    let showBtn = document.getElementById("myCheck")
-
-    showBtn.style.display = "block" 
+function teste(){
+    let task = document.getElementById('task-form')
+    task.style.background = "blue"
+    console.log("deu certo")
 }
+
+window.addEventListener("load",inicia)
 
 /*function addTask(){
 
